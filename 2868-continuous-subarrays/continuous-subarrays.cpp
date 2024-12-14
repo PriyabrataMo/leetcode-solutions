341947fbee6156dfc3e946dc74f9cbebffe78e9d
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2024-12-14 18:03:30 +0530

    Time: 480 ms (5.09%) | Memory: 156.1 MB (7.00%) - LeetSync

---
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        long long ans = 1;
+        int l = 0;
+        int r = 1;
+        int n = nums.size();
+        multiset<int> ms;
+        ms.insert(nums[0]);
+        while (r < n) {
+            int maxi = *ms.rbegin();
+            int mini = *ms.begin();
+            while (l < n && l < r &&
+                   (abs(maxi - nums[r]) > 2 || abs(mini - nums[r]) > 2)) {
+                
+                auto it = ms.find(nums[l]);
+                
+                ms.erase(it);
+                if(ms.size()==0) break;
+                maxi = *ms.rbegin();
+                mini = *ms.begin();
+                l++;
+            }
+            ms.insert(nums[r]);
+            long long sz = ms.size();
+            ans += sz;
+            cout<<ans<<endl;
+            r++;
+        }
+        return ans;
+    }
+};
