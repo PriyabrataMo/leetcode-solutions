03c192efb4dce54b08818632e314d889de2b12c6
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2024-12-31 13:16:18 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (40.49%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -7,20 +7,13 @@ public:
         for(int i=1;i<n;i++){
             int cost = costs[0]+dp[i-1];
             int ind7 = lower_bound(days.begin(),days.end(),days[i]-6)- days.begin();
-            
             if(ind7!=0)cost = min(cost , dp[ind7-1]+costs[1]);
             else cost = min(cost ,costs[1]);
-            
-
-            
             int ind30 = lower_bound(days.begin(),days.end(),days[i]-29)- days.begin();
-            
             if(ind30!=0)cost = min(cost , dp[ind30-1]+costs[2]);
             else cost = min(cost ,costs[2]);
-            
-            
             dp[i] = cost;
-            cout<<ind7<<" "<<ind30<<" "<<dp[i]<<endl;
+            //cout<<ind7<<" "<<ind30<<" "<<dp[i]<<endl;
         }
         return dp[n-1];
     }
