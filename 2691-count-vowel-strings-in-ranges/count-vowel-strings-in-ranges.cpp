7ea54d6b341a1847b69776b032abcd778696a532
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-02 10:02:54 +0530

    Time: 0 ms (100.00%) | Memory: 69.2 MB (74.20%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    
     vector<int> vowelStrings(vector<string>& arr, vector<vector<int>>& q) {
         int n = arr.size();
-        vector<int> vo(n,0); 
-       
-        for(int i=0;i<n;i++){
+        vector<int> vo(n, 0);
+
+        for (int i = 0; i < n; i++) {
             char c = arr[i][0];
-            bool f1 = c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
+            bool f1 = c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
             c = arr[i].back();
-            bool f2 = c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
-            
-            if(f1&&f2){
+            bool f2 = c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+
+            if (f1 && f2) {
                 vo[i] = 1;
             }
-            if(i>0){
-                vo[i]+=vo[i-1];
+            if (i > 0) {
+                vo[i] += vo[i - 1];
             }
         }
-        
-        vector<int> ans;
-        for(auto it:q){
-           
-            if(it[0]>0){
-                ans.push_back(vo[it[1]]-vo[it[0]-1]);
-            }
-            else{
-                ans.push_back(vo[it[1]]);
+
+        vector<int> ans(q.size());
+        for (int i=0;i<q.size();i++) {
+
+            if (q[i][0] > 0) {
+                ans[i]=(vo[q[i][1]] - vo[q[i][0] - 1]);
+            } else {
+                ans[i]=(vo[q[i][1]]);
             }
         }
         return ans;
