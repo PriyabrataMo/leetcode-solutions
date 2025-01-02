fe328ffaa61a4abda201409718c30eb1be65045c
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-02 09:59:23 +0530

    Time: 20 ms (37.19%) | Memory: 74.2 MB (29.89%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -8,13 +8,21 @@ public:
         vector<int> vo(n,0); 
        
         for(int i=0;i<n;i++){
-            if(isvowel(arr[i][0])&&isvowel(arr[i].back())){
+            char c = arr[i][0];
+            bool f1 = c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
+            c = arr[i].back();
+            bool f2 = c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
+            // if(isvowel(arr[i][0])&&isvowel(arr[i].back())){
+            //     vo[i] = 1;
+            // }
+            if(f1&&f2){
                 vo[i] = 1;
             }
+            if(i>0){
+                vo[i]+=vo[i-1];
+            }
         }
-        for(int i=1;i<n;i++){
-            vo[i] += vo[i-1];
-        }
+        
         vector<int> ans;
         for(auto it:q){
             int st = it[0];
