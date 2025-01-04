8cfe7a89c7657a06e14cc5b79fbfdc9ab4e5ab40
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-04 10:47:09 +0530

    Time: 11 ms (99.08%) | Memory: 16.2 MB (40.49%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -3,21 +3,40 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<pair<int,int>> fre(26 , {-1,-1});
+        // vector<pair<int,int>> fre(26 , {-1,-1});
 
         int ans = 0;
         int n = s.size();
-        for(int i=0;i<n;i++){
-            if(fre[s[i]-'a'].ff==-1){
-                fre[s[i]-'a'].ff = i;
+        // for(int i=0;i<n;i++){
+        //     if(fre[s[i]-'a'].ff==-1){
+        //         fre[s[i]-'a'].ff = i;
+        //     }
+        //     fre[s[i]-'a'].ss = i;
+        // }
+        // for(auto it:fre){
+        //     if(it.ss>it.ff&&it.ff!=-1){
+        //         //cout<<it.ss<<" "<<it.ff<<endl;
+        //         ans+= unordered_set(s.begin()+it.ff+1 , s.begin()+it.ss).size();
+        //     }
+        // }
+        for(int i=0;i<26;i++){
+            int l = s.find(i+'a');
+            if(l!=string::npos){
+                int r = s.find_last_of(i+'a');
+                set<char> st;
+                if(r!=string::npos){
+                    if(l+1>=r) continue;
+                    l++;
+                    while(l<r&&st.size()<26){
+                        st.insert(s[l]);
+                        l++;
+
+                    }
+                    ans+=st.size();
+                }
+
             }
-            fre[s[i]-'a'].ss = i;
-        }
-        for(auto it:fre){
-            if(it.ss>it.ff&&it.ff!=-1){
-                //cout<<it.ss<<" "<<it.ff<<endl;
-                ans+= unordered_set(s.begin()+it.ff+1 , s.begin()+it.ss).size();
-            }
+
         }
         return ans;
 
