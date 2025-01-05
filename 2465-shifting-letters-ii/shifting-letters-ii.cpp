3b3f1677164b2b0985ec4ec284e225633d6dc71e
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-05 12:34:08 +0530

    Time: 46 ms (37.82%) | Memory: 108.2 MB (27.49%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& arr) {
         int n = s.size();
-        vector<int> mpp(n+2 , 0);
-        for(auto it:arr){
+        vector<int> mpp(n + 2, 0);
+        for (auto it : arr) {
             int shift = 1;
-            if(it[2]==0){
-                shift = 25;
+            if (it[2] == 0) {
+                shift = -1;
             }
-            mpp[it[0]]+=shift;
-            mpp[it[1]+1]-=shift;
+            mpp[it[0]] += shift;
+            mpp[it[1] + 1] -= shift;
         }
-        
+
         int pre = 0;
-        for(int i=0;i<n;i++){
-            pre+=mpp[i];
-            
-            char rep = (((s[i]-'a')+pre)%26)+'a';
+        for (int i = 0; i < n; i++) {
+            pre = (pre + mpp[i]) % 26;
+
+            char rep = (((s[i] - 'a') + pre + 26) % 26) + 'a';
             s[i] = rep;
         }
         return s;
     }
-   
 };
