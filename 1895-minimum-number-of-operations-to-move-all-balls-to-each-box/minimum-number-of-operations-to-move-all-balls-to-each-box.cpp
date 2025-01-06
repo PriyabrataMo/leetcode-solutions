d269d3c1ba02379940803d7e49e364e1b0b92449
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-06 09:59:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (50.97%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     vector<int> minOperations(string arr) {
         int n = arr.length();
-        vector<int> pre(n,0) , suf(n,0);
-        
-        vector<int>ans (n,0);
-        if(arr[0]=='1')pre[0] = 1;
-        int s =0;
-        for(int i=1;i<n;i++){
-            s+=pre[i-1];
-            if(arr[i]=='1')pre[i] = pre[i-1]+1;
-            else pre[i] = pre[i-1];
-            ans[i] += s;
-            
+        int presum = 0, sufsum = 0;
+        vector<int> ans(n, 0);
+        int b = 0;
+        if (arr[0] == '1')
+            b = 1;
+
+        for (int i = 1; i < n; i++) {
+            presum += b;
+            ans[i] += presum;
+            if (arr[i] == '1')
+                b++;
         }
-        s=0;
-        if(arr[n-1]=='1')suf[n-1] = 1;
-        for(int i=n-2;i>=0;i--){
-            s+=suf[i+1];
-            if(arr[i]=='1')suf[i] = suf[i+1]+1;
-            else suf[i] = suf[i+1];
-            ans[i]+=s;
+        b = 0;
+        if (arr[n - 1] == '1')
+            b = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            sufsum += b;
+            ans[i] += sufsum;
+            if (arr[i] == '1')
+                b++;
         }
         return ans;
     }
