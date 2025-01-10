15dbc802c1843a1ea7ecab8396f266927509bcf9
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-10 23:36:58 +0530

    Time: 67 ms (46.58%) | Memory: 106.7 MB (56.16%) - LeetSync

--- 952-word-subsets/word-subsets.cpp
+++ 952-word-subsets/word-subsets.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    unordered_map<char, int> mpp;
-    bool func(unordered_map<char, int> &mp) {
-        for (auto &itt : mpp) {
+    vector<int> mpp;
+    bool func(vector<int>& mp) {
+        for (int i = 0; i < 26; i++) {
 
-            if (mp[itt.first] < mpp[itt.first]) {
+            if (mp[i] < mpp[i]) {
                 return false;
             }
         }
@@ -12,23 +12,31 @@ public:
     }
     vector<string> wordSubsets(vector<string>& a, vector<string>& b) {
         vector<string> ans;
-
-        for (auto &it : b) {
-            unordered_map<char, int> mp;
-            for (auto &itt : it) {
-                mp[itt]++;
+        mpp.resize(26,0);
+        for (auto& it : b) {
+            vector<int> mp(26,0);
+            for (auto& itt : it) {
+                mp[itt - 'a']++;
             }
-            for(auto &it:mp){
-                mpp[it.first] = max(mp[it.first] , mpp[it.first]);
+            for (int i = 0; i < 26; i++) {
+                mpp[i] = max(mp[i], mpp[i]);
             }
         }
-        
-        for (auto &it : a) {
-            unordered_map<char, int> mp;
-            for (auto &itt : it) {
-                mp[itt]++;
+        // for (auto it : mpp) {
+        //     cout << it << " ";
+        // }
+        //cout << endl;
+
+        for (auto& it : a) {
+            vector<int> mp2(26,0);
+            for (auto& itt : it) {
+                mp2[itt - 'a']++;
             }
-            if(func(mp)){
+            // for (auto it : mp2) {
+            //     cout << it << " ";
+            // }
+            // cout << endl;
+            if (func(mp2)) {
                 ans.push_back(it);
             }
         }
