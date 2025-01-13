462613bc718ec9c19031c9cf0b7d59cc2965f702
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-13 22:33:44 +0530

    Time: 7 ms (93.55%) | Memory: 32.1 MB (58.06%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector<int> fre(26,0);
+        for(auto it:s){
+            fre[it-'a']++;
+        }
+        int ans = 0;
+        for(auto it:fre){
+            if(it>=2){
+                if(it%2)ans+=1;
+                else ans+=2;
+            }
+            else ans+=it;
+        }
+        return ans;
+    }
+};
