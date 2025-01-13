ff284c93a176e8b1497cecf1c635e58a0b44b87f
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-13 22:40:46 +0530

    Time: 81 ms (35.89%) | Memory: 32.1 MB (74.19%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        vector<int> fre(26, 0);
-        for (auto &it : s) {
+        vector<int> fre(26,0);
+        for (auto& it : s) {
             fre[it - 'a']++;
         }
+        
         int ans = 0;
         for (auto& it : fre) {
-            if (it >= 2) {
-                if (it % 2)
-                    ans += 1;
-                else
-                    ans += 2;
-            } else
-                ans += it;
+            cout<<it<<endl;
+            if (it % 2)
+                ans += 1;
+            else
+                ans += it?2:0;
+
+            cout<<ans<<endl;
         }
         return ans;
     }
