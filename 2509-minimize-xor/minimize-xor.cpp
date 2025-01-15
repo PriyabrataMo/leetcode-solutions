edf3cb5e5987c99975636f6fd15385d6a34cc80d
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-15 23:50:13 +0530

    Time: 15 ms (2.34%) | Memory: 8.1 MB (10.75%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        bitset<32> n2(num2);
+        bitset<32> n1(num1);
+        int one1 = n1.count();
+        int one2 = n2.count();
+        cout<<one1<<" "<<one2<<endl;
+        if(one1==one2) return num1;
+        bitset<32> ans;
+        for(int i=31;i>=0;i--){
+            cout<<i<<endl;
+            if(n1[i]==1&&one2>0){
+                
+                one2--;
+                ans[i] = 1;
+            }
+            
+        }
+        for(int i=0;i<=31;i++){
+            if(n1[i]==0&&one2>0){
+                one2--;
+                ans[i] = 1;
+            }
+        }
+        return  static_cast<int>(ans.to_ulong());
+
+    }
+};
