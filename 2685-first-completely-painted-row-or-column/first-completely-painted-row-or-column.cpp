4baa43180730bc40594101685c66658f84f3ad72
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-20 19:57:21 +0530

    Time: 86 ms (77.13%) | Memory: 161.9 MB (67.68%) - LeetSync

---
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> pos((n*m)+1 , vector<int>(2,-1));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                pos[mat[i][j]][0] = i;
+                pos[mat[i][j]][1] = j;
+            }
+        }
+        vector<int> ro(n,0) , co(m,0);
+        for(int i=0;i<arr.size();i++){
+            int r = pos[arr[i]][0];
+            int c = pos[arr[i]][1];
+            ro[r]++;
+            co[c]++;
+            if(ro[r]>=m||co[c]>=n) return i;
+        }
+        return -1;
+    }
+};
