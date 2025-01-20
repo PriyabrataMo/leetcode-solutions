53abd7e1c1ac02fb03e0fcb02effad1e24b43fa7
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-20 20:00:03 +0530

    Time: 5 ms (92.68%) | Memory: 133.9 MB (91.77%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -3,18 +3,18 @@ public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        vector<vector<int>> pos((n*m)+1 , vector<int>(2,-1));
+        vector<pair<int,int>> pos((n*m)+1);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                pos[mat[i][j]][0] = i;
-                pos[mat[i][j]][1] = j;
+                pos[mat[i][j]].first = i;
+                pos[mat[i][j]].second = j;
             }
         }
         int sz = arr.size();
         vector<int> ro(n,0) , co(m,0);
         for(int i=0;i<sz;i++){
-            int r = pos[arr[i]][0];
-            int c = pos[arr[i]][1];
+            int r = pos[arr[i]].first;
+            int c = pos[arr[i]].second;
             
             if(++ro[r]>=m||++co[c]>=n) return i;
         }
