7b4151be9b1aa1e83a1f0c119207e580d167aa09
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-23 12:44:03 +0530

    Time: 0 ms (100.00%) | Memory: 26.7 MB (59.58%) - LeetSync

---
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<int> ro(n,0) , co(m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    ro[i]++;
+                    co[j]++;
+                }
+            }
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]&&(ro[i]>1||co[j]>1)){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+
+    }
+};
