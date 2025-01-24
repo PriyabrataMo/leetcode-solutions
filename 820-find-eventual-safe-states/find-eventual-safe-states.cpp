b905cda5e450c78e647ed1fc858092760194b4c4
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-24 11:41:55 +0530

    Time: 51 ms (43.21%) | Memory: 66.8 MB (10.10%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -16,17 +16,21 @@ public:
             }
             
         }
-        vector<int> ans;
+        vector<int> an(n,0);
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            ans.push_back(node);
+            an[node]=1;
             for(auto &it:adj[node]){
                 indeg[it]--;
                 if(indeg[it]==0)q.push(it);
             }
         }
-        sort(ans.begin() , ans.end());
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(an[i]==1)ans.push_back(i);
+        }
+        
         return ans;
     }
 };
