f152a595a71e1abb9915916ce5ec4e78c229abb2
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-01-30 11:37:34 +0530

    Time: 2 ms (18.72%) | Memory: 17.5 MB (27.88%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,46 +1,42 @@
 class Solution {
 public:
-    set<int> a, b;
-    void bfs(int src ,vector<int>& vis, vector<vector<int>>& graph){
+    
+    bool bfs(int src ,vector<int>& vis, vector<vector<int>>& graph){
         vis[src]=1;
         queue<int> q;
         q.push(src);
-        a.insert(src);
         while (!q.empty()) {
             int node = q.front();
 
             q.pop();
-            cout<<node<<endl;
-            if (a.find(node) == a.end()) {
-                for (auto it : graph[node]) {
-                    a.insert(it);
-                    if (vis[it] == 0) {
-                        q.push(it);
-                        vis[it] = 1;
-                    }
+            for(auto it:graph[node]){
+                if(vis[it]!=-1){
+                    if(vis[it]==vis[node]) return true;
                 }
-            } else {
-                for (auto it : graph[node]) {
-                    b.insert(it);
-                    if (vis[it] == 0) {
-                        q.push(it);
-                        vis[it] = 1;
-                    }
+                else{
+                    q.push(it);
+                    vis[it]=vis[node]?0:1;
                 }
             }
+            
+                   
         }
+        return false;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         
         int n = graph.size();
 
-        vector<int> vis(n, 0);
+        vector<int> vis(n, -1);
         for(int i=0;i<n;i++){
-            if(vis[i]==0)bfs(i , vis , graph);
+            if(vis[i]==-1&&bfs(i , vis , graph)) return false;
         }
-        for(int i=0;i<n;i++){
-            if(a.find(i)!=a.end()&&b.find(i)!=b.end())return false;
+        for(auto it:vis){
+            cout<<it<<endl;
         }
+        // for(int i=0;i<n;i++){
+        //     if(a.find(i)!=a.end()&&b.find(i)!=b.end())return false;
+        // }
         return true;
     }
 };
