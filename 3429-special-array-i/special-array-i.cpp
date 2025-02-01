d34a516a1056f9e99dd7e8244284fdf00db945cc
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-02-01 10:48:53 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (25.00%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -3,7 +3,7 @@ public:
     bool isArraySpecial(vector<int>& nums) {
         int n = nums.size();
         for(int i=1;i<n;i++){
-            if(((nums[i]&1)^(nums[i-1]&1))==0)return false;
+            if((nums[i-1]%2)==(nums[i]%2))return false;
         }
         return true;
     }
