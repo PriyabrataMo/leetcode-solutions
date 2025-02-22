8665106e1c891939672190c2bfad33510a0b0ae8
Priyabrata Mondal <95865224+PriyabrataMo@users.noreply.github.com>
2025-02-22 21:00:57 +0530

    Time: 31 ms (9.63%) | Memory: 21.8 MB (44.64%) - LeetSync

---
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* recoverFromPreorder(string s) {
+        map<int,vector<TreeNode*>> mpp;
+        int dep=0;
+        int n = s.length();
+        int num=0;
+        int i=0;
+        while(i<n&&s[i]!='-'){
+            num = num*10+(s[i]-'0');
+            i++;
+        }
+        TreeNode* root = new TreeNode(num);
+        mpp[0].push_back(root);
+        
+        for(;i<n;i++){
+            if(s[i]=='-'){
+                dep++;
+            }
+            else{
+                num=0;
+                while(i<n&&s[i]!='-'){
+                    num = num*10+(s[i]-'0');
+                    i++;
+                }
+                // cout<<i<<endl;
+                TreeNode* newNode = new TreeNode(num);
+                mpp[dep].push_back(newNode);
+                // // cout<<dep<<endl;
+                if(mpp[dep-1].back()->left==NULL){
+                    mpp[dep-1].back()->left=newNode;
+                }
+                else{
+                    mpp[dep-1].back()->right=newNode;
+                }
+                dep=1;
+            }
+        }
+        for(auto it:mpp){
+            cout<<it.first<<endl;
+            for(auto itt:it.second){
+
+                cout<<itt->val<<" ";
+            }
+            cout<<endl;
+            // cout<<"dhjfs"<<endl;
+        }
+        return root;
+    }
+};
